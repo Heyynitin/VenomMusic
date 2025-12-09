a80011917301cc847c5cc229cd4fbeaaeea8cf30
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-12-09 05:54:51 +0530

    Update Youtube.py

--- VenomMusic/platforms/Youtube.py
+++ VenomMusic/platforms/Youtube.py
@@ -6,7 +6,7 @@ from typing import Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from py_yt import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 from VenomMusic.utils.formatters import time_to_seconds
 import aiohttp
 from VenomMusic import LOGGER
